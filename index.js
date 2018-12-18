99d9637bb07c78222dbac8dce898ff29af13d0c6
winzard <ridzwan.midris@gmail.com>
2018-12-18 13:06:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ function takeANumber(line, customer) {
   return `Welcome, ${customer}. You are number ${line.length} in line.`
 }
 
+function nowServing(line) {
+  var first = line[0]
+  if (line.length > 0) {
+    line.shift()
+  } else {
+    return "There is nobody waiting to be served!"
+  }
+  return first
+}
