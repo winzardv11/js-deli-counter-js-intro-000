bb5fc2201d67e54ad4445d654903a8c53c426618
winzard <ridzwan.midris@gmail.com>
2018-12-18 13:58:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function takeANumber(line, customer) {
 
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
-    return 'There si nobody waiting to be served!'
+    return 'There is nobody waiting to be served!'
   }
   return `Currently serving ${katzDeliLine.shift()}.`
 }
